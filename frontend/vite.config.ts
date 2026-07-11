9af4ce0ca8c1f93d75427950e7161cda33095c31
Sminp <0102262@gmail.com>
2026-07-11 22:55:00 +0900

    [chore] vitest 설치

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,8 +10,11 @@ export default defineConfig({
     port: 3000,
   },
   resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve(__dirname, 'src') },
-    ],
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  test: {
+    globals: true, // describe/it/expect 임포트 없이 사용
+    environment: 'jsdom', // DOM + localStorage
+    setupFiles: './src/test-setup.ts',
   },
 });
